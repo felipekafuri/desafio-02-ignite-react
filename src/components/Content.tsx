0b8be03d9b0d11b180d8a5c8c8e52c953475eb64
felipekafuri <felipekafuri@discente.ufg.br>
2021-03-19 14:53:59 -0300

    Componentizando a aplicação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,20 @@
-export function Content() {
-  // Complete aqui
+import { useState } from 'react';
+import { MovieProps } from '../App';
+import {MovieCard} from './MovieCard'
+
+interface ContentProps{
+  movies: MovieProps[];
+}
+
+export function Content({movies}:ContentProps) {
+
+return(
+  <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+)
 }
